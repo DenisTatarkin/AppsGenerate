12b0696f15879d2001fb77976d248f057d01598a
Denis Tatarkin <denistatarkin11@gmail.com>
2020-05-05 16:18:35 +0300

    feat: migrations

---
+++ AppsGenerate/CodeGenerate/Generators/ServerSide/DbMigrationGenerator.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using AppsGenerate.CodeGenerate.Services;
+using AppsGenerate.Structures;
+using AppsGenerate.Structures.Impl;
+
+namespace AppsGenerate.CodeGenerate.Generators
+{
+    public class DbMigrationGenerator : ServerGenerator
+    {
+        public override FileInfo Generate(Structure structure)
+        {
+            var entityStructure = structure as EntityStructure;
+            
+            using (var fs = File.Create($"{structure.Name}Migration.cs"))
+            using (var writer = new StreamWriter(fs))
+            {
+                writer.WriteLine($"namespace {entityStructure.Project.Name}.Migration{{");
+                writer.WriteLine(UsingsService.GetUsings());
+                writer.WriteLine($"[Migration({DateTime.Now.Year}{DateTime.Now.Day}{DateTime.Now.Month}{DateTime.Now.Hour}{DateTime.Now.Minute}00)]");
+                writer.WriteLine($"public class {structure.Name}Migration:Migration{{");
+                writer.WriteLine($"public Up(){{");
+                writer.WriteLine($"Create.Table(\"{structure.Name}\")");
+                writer.WriteLine(".WithColumn(\"Id\").AsInt64().PrimaryKey().Identity");
+                entityStructure.GetProperties().ForEach(x => writer.WriteLine($".WithColumn(\"{x.Name}\").As{x.Type}()"));
+                writer.WriteLine(";}}");
+            }
+
+            return new FileInfo($"{structure.Name}Migration.cs");
+ 
+        }
+    }
+}
